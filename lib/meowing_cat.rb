0d0b192c293983b98b52c55d7f640209da3155f0
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-04 23:36:22 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+class Cat
+  attr_reader :name 
+  attr_writer :name
+end
+
+class Cat
+  def meow
+    puts "meow!"
+  end
+end
+
+class Cat
+  attr_accessor :name
+end
